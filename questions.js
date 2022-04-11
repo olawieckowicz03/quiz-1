1848d4ff0b840fcb34156162d45e2b501c158978
olawieckowicz03 <81527812+olawieckowicz03@users.noreply.github.com>
2022-04-11 11:21:05 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,80 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Skąd pochodzi znak pisarski @(małpa)?",
+    answer: "z języka łacińskiego",
+    options: [
+      "z języka arabskiego",
+      "z języka greckiego",
+      "z języka łacińskiego",
+      "z języka małpiego"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Jak informatyk nazywa zamknięcie butelki?",
+    answer: "Kapslock",
+    options: [
+      "Zakrętka",
+      "Korek",
+      "Nakrętka",
+      "Kapslock"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Jaka jest ulubiona część garderoby programisty?",
+    answer: "Stringi",
+    options: [
+      "Elegancka koszula",
+      "Stringi",
+      "Krawat",
+      "Jeansy"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Co mają wspólnego informatyk i blondynka?",
+    answer: "Oboje żyją dzięki sztucznej inteligencji",
+    options: [
+      "Oboje żyją dzięki sztucznej inteligencji",
+      "Oboje potrafią programować w JavaScript",
+      "Oboje umieją naprawić komputer",
+      "Oboje znają się na komputerach"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Jak informatycy najczęściej zwracają się do swoich żon?",
+    answer: "Myszko",
+    options: [
+      "Pączusiu",
+      "Myszko",
+      "Misiu",
+      "Skarbie"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Po jaką witaminę przychodzi informatyk do apteki?",
+    answer: "Witaminę C++",
+    options: [
+      "Witaminę C++",
+      "Witaminę D",
+      "Witaminę C",
+      "Witaminę A"
+    ]
+  },
+    {
+      numb: 7,
+      question: "Kto jest najlepszym informatykiem wszech czasów?",
+      answer: "Bogusław Gorczyca",
+      options: [
+        "Dennis Ritchie",
+        "Bjarne Stroustrup",
+        "Guido van Rossum",
+        "Bogusław Gorczyca"
+      ]
+  },
+
+];
